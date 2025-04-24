0ebb989536dae72e991aa967824746564515f99f
Jim Vergara <vergarajim.info@gmail.com>
2025-04-24 23:26:05 +0200

    feat: Added PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,44 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      workbox: {
+        maximumFileSizeToCacheInBytes: 10000000, // ~5 MB, increase as needed
+      },
+      manifest: {
+        name: "Hanzi Typer",
+        short_name: "App",
+        description: "Hanzi typer app, type hanzi i guess?",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/hanzi-typer/",
 });
